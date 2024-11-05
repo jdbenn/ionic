26c75934eb7a0a34b4d57dd3805569cb54b3bc78
John Bennett <john.bennett@prismhr.com>
2024-11-04 19:32:49 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initServicesFactory } from "./util";
+import {PhotoService} from "./services/photo.service";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: APP_INITIALIZER,
+      useValue: initServicesFactory,
+      deps: [PhotoService],
+      multi: true
+    }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/util.ts
@@ -0,0 +1,7 @@
+﻿import {PhotoService} from "./services/photo.service";
+
+export function initServicesFactory(photoService: PhotoService) {
+  return () => {
+    return photoService.load().then(photoServices => {});
+  }
+}
